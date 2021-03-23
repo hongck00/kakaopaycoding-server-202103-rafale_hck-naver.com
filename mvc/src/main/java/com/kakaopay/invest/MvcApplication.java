1dd619ede5e66bfdb1711f622bda2a3d8d610b8c
hongck00 <hongck00@gmail.com>
2021-03-24 00:21:08 +0900

    logging 세팅중

--- mvc/src/main/java/com/kakaopay/invest/MvcApplication.java
+++ mvc/src/main/java/com/kakaopay/invest/MvcApplication.java
@@ -1,12 +1,19 @@
 package com.kakaopay.invest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MvcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MvcApplication.class, args);
+        log.info("info");
+        log.debug("debug");
+        log.warn("warn");
+        log.error("error");
+        log.trace("trace");
     }
 }
