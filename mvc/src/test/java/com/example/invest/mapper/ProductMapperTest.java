b8eaf094d6e501ee6501c157a39d71a8581fb43e
ckhong <ckhong@enliple.com>
2021-04-06 19:03:42 +0900

    test

--- mvc/src/test/java/com/example/invest/mapper/ProductMapperTest.java
+++ mvc/src/test/java/com/example/invest/mapper/ProductMapperTest.java
@@ -1,17 +1,27 @@
 package com.example.invest.mapper;
 
+import com.example.invest.config.DataSourceConfig1;
+import com.example.invest.config.DataSourceConfig2;
+import com.example.invest.dto.ProductInvestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProductMapperTest {
 
+    @Autowired private ProductMapper productMapper;
+
     @Test
     @DisplayName("상품 전체 조회")
     public void selectProductAll() {
         System.out.println("#########");
+        List<ProductInvestDto> list =  productMapper.selectProductAll();
+        System.out.println("ls size >> " + list.size());
     }
 }
